827f0c456a3bfcc2c909b3e8bbcbc482bbc16458
İzzet Kılıç <izzetkalic@gmail.com>
2022-04-09 19:03:17 +0300

    fix

--- examples/pytorch/question-answering/run_qa.py
+++ examples/pytorch/question-answering/run_qa.py
@@ -46,9 +46,8 @@ from transformers.utils import check_min_version
 from transformers.utils.versions import require_version
 from utils_qa import postprocess_qa_predictions
 
-
 # Will error if the minimal version of Transformers is not installed. Remove at your own risks.
-#check_min_version("4.19.0.dev0")
+# check_min_version("4.19.0.dev0")
 
 require_version("datasets>=1.8.0", "To fix: pip install -r examples/pytorch/question-answering/requirements.txt")
 
@@ -82,7 +81,7 @@ class ModelArguments:
         default=False,
         metadata={
             "help": "Will use the token generated when running `transformers-cli login` (necessary to use this script "
-            "with private models)."
+                    "with private models)."
         },
     )
 
@@ -119,36 +118,36 @@ class DataTrainingArguments:
         default=384,
         metadata={
             "help": "The maximum total input sequence length after tokenization. Sequences longer "
-            "than this will be truncated, sequences shorter will be padded."
+                    "than this will be truncated, sequences shorter will be padded."
         },
     )
     pad_to_max_length: bool = field(
         default=True,
         metadata={
             "help": "Whether to pad all samples to `max_seq_length`. "
-            "If False, will pad the samples dynamically when batching to the maximum length in the batch (which can "
-            "be faster on GPU but will be slower on TPU)."
+                    "If False, will pad the samples dynamically when batching to the maximum length in the batch (which can "
+                    "be faster on GPU but will be slower on TPU)."
         },
     )
     max_train_samples: Optional[int] = field(
         default=None,
         metadata={
             "help": "For debugging purposes or quicker training, truncate the number of training examples to this "
-            "value if set."
+                    "value if set."
         },
     )
     max_eval_samples: Optional[int] = field(
         default=None,
         metadata={
             "help": "For debugging purposes or quicker training, truncate the number of evaluation examples to this "
-            "value if set."
+                    "value if set."
         },
     )
     max_predict_samples: Optional[int] = field(
         default=None,
         metadata={
             "help": "For debugging purposes or quicker training, truncate the number of prediction examples to this "
-            "value if set."
+                    "value if set."
         },
     )
     version_2_with_negative: bool = field(
@@ -158,8 +157,8 @@ class DataTrainingArguments:
         default=0.0,
         metadata={
             "help": "The threshold used to select the null answer: if the best answer has a score that is less than "
-            "the score of the null answer minus this threshold, the null answer is selected for this example. "
-            "Only useful when `version_2_with_negative=True`."
+                    "the score of the null answer minus this threshold, the null answer is selected for this example. "
+                    "Only useful when `version_2_with_negative=True`."
         },
     )
     doc_stride: int = field(
@@ -174,16 +173,16 @@ class DataTrainingArguments:
         default=30,
         metadata={
             "help": "The maximum length of an answer that can be generated. This is needed because the start "
-            "and end predictions are not conditioned on one another."
+                    "and end predictions are not conditioned on one another."
         },
     )
 
     def __post_init__(self):
         if (
-            self.dataset_name is None
-            and self.train_file is None
-            and self.validation_file is None
-            and self.test_file is None
+                self.dataset_name is None
+                and self.train_file is None
+                and self.validation_file is None
+                and self.test_file is None
         ):
             raise ValueError("Need either a dataset name or a training/validation file/test_file.")
         else:
@@ -390,13 +389,6 @@ def main():
             sample_index = sample_mapping[i]
             answers = examples[answer_column_name][sample_index]
             # If no answers are given, set the cls_index as answer.
-            print('EXAMPLESSS=????????????')
-            print(examples[answer_column_name])
-            print('SAMPLE_INDEX=????????????')
-            print(sample_index)
-            print('ANSWERSSS=????????????')
-            print(answers)
-            answers=answers[0]
             if len(answers["answer_start"]) == 0:
                 tokenized_examples["start_positions"].append(cls_index)
                 tokenized_examples["end_positions"].append(cls_index)
